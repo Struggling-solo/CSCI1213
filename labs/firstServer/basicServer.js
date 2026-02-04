fff46c48a76256f000d867934c1be532f50c737f
Deborah Hanson <dddh@gmx.com>
2026-02-04 13:08:14 -0700

    Feb 4 lab modifications

--- labs/firstServer/basicServer.js
+++ labs/firstServer/basicServer.js
@@ -1,11 +1,21 @@
 const http = require('http');
+const fs = require('fs')
+
 
 const server = http.createServer((req,res) => {
-    // console.log(`${req.method} ${req.url}`)
+    console.log(`${req.method} ${req.url}`)
 
-    res.writeHead(200, {'content-type': 'text/html' });
-    res.end('<h1>Hello, World!<h1>');
-
+    if (req.url === '/') {
+        res.writeHead(200, {'content-type': 'text/html' });
+        res.end(fs.readFileSync('index.html'));
+    }
+    else if (req.url === '/about') {
+        res.writeHead(200, {'content-type': 'text/html' });
+        res.end(fs.readFileSync('about.html'));
+    }
+    else {
+        res.writeHead(200, {'content-type': 'text/html' }); 
+        res.end(fs.readFileSync('notFound.html'));   }
 });
 
 server.listen(3000, () => {
