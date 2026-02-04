93f249bd05715068c5ba5c446eefc08d6ecdb0e6
Deborah Hanson <dddh@gmx.com>
2026-02-04 12:07:37 -0700

    remove end

--- labs/firstServer/basicServer.js
+++ labs/firstServer/basicServer.js
@@ -5,6 +5,7 @@ const server = http.createServer((req,res) => {
 
     res.writeHead(200, {'content-type': 'text/html' });
     res.end('<h1>Hello, World!<h1>');
+
 });
 
 server.listen(3000, () => {
